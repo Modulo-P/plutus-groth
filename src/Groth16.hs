7040bcd63f4223588549c022c9803b5a78e8cbcf
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2023-11-24 08:26:15 +0100

    refactor: folder structure

---
+++ src/Groth16.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Groth16 where
+
+import           PlutusTx         as Tx
+import           PlutusTx.Prelude
+import qualified PlutusTx.Show    as Tx
+
+data VerificationKey = VerificationKey {
+    nPublic     :: Integer
+  , vkAlpha     :: [Integer]
+  , vkBeta      :: [[Integer]]
+  , vkGamma     :: [[Integer]]
+  , vkDelta     :: [[Integer]]
+  , vkAlphaBeta :: [[[Integer]]]
+  }
+
+Tx.deriveShow ''VerificationKey
